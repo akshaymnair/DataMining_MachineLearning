7de7976a8f013aec81ed9890edfc12c0e0b1baa7
boscofrankpaul <bfpaul@asu.edu>
2017-11-25 20:17:44 -0700

    Task 3 Complete

--- Code/task3/lsh.py
+++ Code/task3/lsh.py
@@ -1,27 +1,20 @@
 import numpy as np
 import pandas as pd
 import math
+from numpy.linalg import norm
 
-layers = 50
-hashes = 10
+layers = 5
+hashes = 3
 P = np.random.normal(size=[layers, hashes, 500])
 w = 2
 b = np.random.random_sample([layers,hashes]) * w
-
-
 hashtable = []
 
-
-
-
-
-def setInitalP():
-	np.random.normal()
 def hashFun(a,l):
 	temp = ""
 	count = dict()
 	for i in xrange(hashes):
-		temp += str(int(math.floor((np.dot(a,P[l][i]) + b[l][i] )/w)))
+		temp += "&" + str(int(math.floor((np.dot(a,P[l][i]) + b[l][i] )/w)))
 	return temp
 
 
@@ -37,25 +30,23 @@ def main():
 				dict_hash[hash_bin] = [mid]
 		hashtable.append(dict_hash)
 
-	q_mid = 3111
-	print 'Printing movies in the same bin as query movie', q_mid
-
+	query_movie = 3111
+	query_r = 10
+	movie_list = []
 	for l in xrange(layers):
-		hash_bin = hashFun(movie_df.loc[q_mid].tolist(),l)
-		print hash_bin
+		hash_bin = hashFun(movie_df.loc[query_movie].tolist(),l)
 		if( hash_bin in hashtable[l] and len(hashtable[l][hash_bin])>1 ):
-			print hashtable[l][hash_bin]
+			movie_list=  movie_list + hashtable[l][hash_bin]
 
-		else:
-			print 'No movies in the bin for layer', l
-	'''for hdict in hashtable:
-		for key in hdict:
-			if(len(hdict[key])>1):
-				print hdict[key]'''
+	print "Number of unique movies considered :- " + str(len(np.unique(movie_list)))
+	print "Total no of movies considered :- " + str(len(movie_list))
+	
+	cosine_movie = pd.DataFrame(0, columns=np.unique(movie_list), index=('CosineValues',))
+	for i in np.unique(movie_list):
+		cosine_movie[i] = np.dot(movie_df.loc[query_movie],movie_df.loc[i]) / (norm(movie_df.loc[query_movie])*norm(movie_df.loc[i]))	
 
-
-
-
+	print cosine_movie.loc['CosineValues'].nlargest(query_r+1)
+	
 
 if __name__ == '__main__':
 	main()
