9f48c7777a9f69d53801b26bfe7fed64204295f1
boscofrankpaul <bfpaul@asu.edu>
2017-11-29 15:23:05 -0700

    Task 5.1

---
+++ Code/task5/rnn.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+r = 10
+movie_df = pd.read_pickle('movie_matrix_svd.pkl')
+user_input = pd.read_csv('movie_labels.csv')
+train_movie = movie_df.ix[ list(user_input['movieid'])]
+cosine_similarity = pd.DataFrame(np.dot(movie_df,train_movie.T),columns = train_movie.index, index = movie_df.index )
+
+for i,row in cosine_similarity.iterrows():
+	if i not in user_input:
+		neighbours = row.nlargest(r).index
+		neighbour_labels =  user_input[user_input['movieid'].isin(neighbours)]
+		print (i,neighbour_labels['label'].value_counts().idxmax())
